ca7c119b2c878f86897f81807b1a98247a849700
Robert Schwieger <Robert.Schwieger@uni-konstanz.de>
2015-03-27 14:14:28 +0100

    installer for dependencies added

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,9 +6,7 @@ cd ~/Downloads/
 sudo apt-get install mercurial
 hg clone https://bitbucket.org/pygame/pygame
 cd pygame
-sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
-  libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev \
-  libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev
+sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev
 python3 setup.py build
 sudo apt-get remove mercurial
 
