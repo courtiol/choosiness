32f20bb891854821c97c82f60a5d68314a62d7e3
courtiol <alexandre.courtiol@gmail.com>
2015-03-27 16:23:49 +0100

    (remove visualisation for tests on cluster... to be continued)

--- simulate.py
+++ simulate.py
@@ -32,6 +32,7 @@ def simulate(settings_file, number_of_iterations, frequency_saving, width_window
 
     # We want to combine several visualizations. Therefore we need to define a list of the ones, that we want.
     # In each visualization we pick the attributes of the individuals, which we like to see
+    # ToDO: include following in gui settings stuff
     used_of_visualizations = [C1Histogram(simulation, width_window, height_window, 'phi'),
                           CNHistograms(simulation, width_window, height_window, ['phi','q']),
                           CAverage(simulation, width_window, height_window, 'phi'),
@@ -39,11 +40,12 @@ def simulate(settings_file, number_of_iterations, frequency_saving, width_window
                           C2DVisualizationOfSimulation(simulation, width_window, height_window)]
 
     # Create a combination of these visualizations. You can iterate through the simulations by using left and right arrow
+    # ToDO: do not initialize the loger in the following class!
     visualization = CCombinationOfVisualizations(simulation, width_window, height_window, used_of_visualizations)
 
-    visualization.init_display()
+    # visualization.init_display()
     # modify the simulation that it can be visualized
-    add_visualization_to_simulation(simulation, visualization)
+    # add_visualization_to_simulation(simulation, visualization)
 
     # Start the simulation
     print("Start simulation")
